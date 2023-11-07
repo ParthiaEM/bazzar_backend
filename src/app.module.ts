cf17318ae5dd0617999b1d2bac27c5086f30426a
HUN1i <nicebrian0328@gmail.com>
2023-11-07 19:21:25 +0900

    ADD :: JWT to Session

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
-import { Module } from '@nestjs/common';
+require('dotenv').config();
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { IdeaModule } from './idea/idea.module';
 import { DatabaseModule } from './database/database.module';
+import { SessionModule } from './auth/session.module';
+
 @Module({
-  imports: [UserModule, IdeaModule, DatabaseModule],
+  imports: [UserModule, IdeaModule, DatabaseModule, SessionModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,21 @@
+require('dotenv').config();
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UserModule } from '../user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+
+@Module({
+  imports: [
+    UserModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.SECRET_KEY,
+      signOptions: { expiresIn: '24h' },
+    }),
+  ],
+  providers: [AuthService],
+  controllers: [AuthController],
+  exports: [AuthService],
+})
+export class AuthModule {}
