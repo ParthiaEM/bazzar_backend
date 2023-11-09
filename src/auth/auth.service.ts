4142568b0854a74e2c33fdb7df73718f8fe6a627
HUN1i <nicebrian0328@gmail.com>
2023-11-09 22:55:30 +0900

    FIX :: 토큰 내 값 수정

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -17,13 +17,10 @@ export class AuthService {
       expiresIn: '1d',
       secret: process.env.SECRET_KEY,
     });
-    return `Authentication=${token}; HttpOnly; Path=/; Max-Age=${this.configService.get(
-      '24h',
-    )}`;
+    return `Authentication=${token}`;
   }
 
   async validateUser(payload: any) {
-    // payload에서 필요한 정보를 추출하여 특정 조건을 확인하고 사용자를 반환합니다.
     const user = await this.userService.findOne(payload.userUniqueId);
     if (user) {
       return user;
